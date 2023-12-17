466611043c8386516fe56faa100bf08fa5845ca8
George Bjork <georgebjork@outlook.com>
2023-12-17 15:24:42 -0800

    basic config added in plugin

--- MoreMusicMod/MoreMusicMod/Plugin.cs
+++ MoreMusicMod/MoreMusicMod/Plugin.cs
@@ -1,12 +1,20 @@
 ﻿using BepInEx;
+using HarmonyLib;
 
 namespace MoreMusicMod
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        private static Plugin _instance;
+        
         private void Awake()
         {
+            _instance = this;
+            
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+            
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
