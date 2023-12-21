de5831210d24c235bbc1bcd82ba915ef7516af28
George Bjork <georgebjork@outlook.com>
2023-12-20 18:18:38 -0800

    Create Settings.cs

---
+++ MoreMusicMod/MoreMusicMod/Configuration/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace MoreMusicMod.Configuration;
+
+public static class Settings
+{
+    public static string PLUGINS_DIRECTORY = "plugins";
+    public static string SONGS_FOLDER = "MoreMusic/Music";
+}
