0b73baf121d300e11b4eda2d85e5dafd25c81c3e
George Bjork <georgebjork@outlook.com>
2023-12-20 18:18:06 -0800

    added audio helper to help determine audio types

---
+++ MoreMusicMod/MoreMusicMod/Helpers/AudioHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace MoreMusicMod.Helpers;
+
+public class AudioHelper
+{
+    public static AudioType GetAudioType(string path)
+    {
+        var extension = Path.GetExtension(path).ToLower();
+
+        if (extension == ".wav")
+            return AudioType.WAV;
+        if (extension == ".ogg")
+            return AudioType.OGGVORBIS;
+        if (extension == ".mp3")
+            return AudioType.MPEG;
+
+        Plugin.LogError($"Unsupported extension type: {extension}");
+        return AudioType.UNKNOWN;
+    }
+}
